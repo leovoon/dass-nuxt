ef894cc225680db98c4c6e4f7ad44ae260cf89a0
leovoon <voonlihhaur@gmail.com>
2022-10-05 10:57:41 +0800

    fix: preload imgs

--- app.vue
+++ app.vue
@@ -23,12 +23,39 @@ useHead({
       content: 'Depression, Anxiety and Stress Scale',
     },
   ],
+
   link: [
     {
       rel: 'icon',
       type: 'image/x-icon',
       href: '/favicon.ico',
     },
+    {
+      rel: 'preload',
+      href: '/emotion/extreme.png',
+      as: 'image',
+    },
+    {
+      rel: 'preload',
+      href: '/emotion/mild.png',
+      as: 'image',
+    },
+    {
+      rel: 'preload',
+      href: '/emotion/moderate.png',
+      as: 'image',
+    },
+    {
+      rel: 'preload',
+      href: '/emotion/normal.png',
+      as: 'image',
+    },
+    {
+      rel: 'preload',
+      href: '/emotion/severe.png',
+      as: 'image',
+    }
+
   ],
 })
 
